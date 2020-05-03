6968c8c767554acea8cb418a48bc7bf1802233c0
kyazuki <20392096+kyazuki@users.noreply.github.com>
2020-05-03 20:13:19 +0900

    Python 3.6 to 3.8

--- stop.py
+++ stop.py
@@ -8,6 +8,6 @@ api.update_profile(name = settings.profile_name_stop, description = settings.pro
 
 #cronを編集し、待機モードに設定
 with open(settings.cronpath, mode = 'w') as f:
-  f.write('#* * * * * for i in `seq 0 10 59`;do (sleep ${i}; cd /home/twitter/www/yojirei_bot/repository; python3.6 run.py) & done;\n* * * * * (cd /home/twitter/www/yojirei_bot/repository; python3.6 standby.py) &\n')
+  f.write('#* * * * * for i in `seq 0 10 59`;do (sleep ${i}; cd /home/twitter/www/yojirei_bot/repository; python3.8 run.py) & done;\n* * * * * (cd /home/twitter/www/yojirei_bot/repository; python3.8 standby.py) &\n')
 for cmd in settings.cron_cmds:
   subprocess.call(cmd.split())
